0d080fc402db6a99ea68bc3eeae839faf8c0abe8
Four <fourtyfive450@gmail.com>
2019-04-04 17:41:48 +0200

    Update FragmentSubscribeMF.java

--- app/src/main/java/com/example/mktabty_attempt3/Fragments/FragmentSubscribeMF.java
+++ app/src/main/java/com/example/mktabty_attempt3/Fragments/FragmentSubscribeMF.java
@@ -1,9 +1,11 @@
 package com.example.mktabty_attempt3.Fragments;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.RecyclerView;
@@ -13,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.mktabty_attempt3.R;
 
@@ -45,15 +48,34 @@ public class FragmentSubscribeMF extends Fragment {
 
     RecyclerView recyclerView;
     DrawerLayout mDrawerLayout;
-
+    TextView label,loongtext;
+    Button avtivate,policy;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_subscribe_mf, container, false);
-
+        Typeface face1 = Typeface.createFromAsset(getActivity().getAssets(),"font2.otf");
+        Typeface face = Typeface.createFromAsset(getActivity().getAssets(),"font2.otf");
+        label = view.findViewById(R.id.textView3);
+        label.setTypeface(face1);
+        avtivate = view.findViewById(R.id.button17);
+        policy=view.findViewById(R.id.button18);
+        loongtext=view.findViewById(R.id.textView234);
+        loongtext.setTypeface(face);
+        avtivate.setTypeface(face);
+        policy.setTypeface(face);
         mDrawerLayout = (DrawerLayout)getActivity().findViewById(R.id.drawer_layout);
         Button button = view.findViewById(R.id.b4);
-
+        policy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentPrivacyPolicy newGamefragment = new FragmentPrivacyPolicy();
+                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.frame, newGamefragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
