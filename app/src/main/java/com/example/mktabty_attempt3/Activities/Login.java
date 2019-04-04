578835201b1aacb987d7ca880a682cf0ddb45ed5
Four <fourtyfive450@gmail.com>
2019-04-04 17:41:01 +0200

    Update Login.java

--- app/src/main/java/com/example/mktabty_attempt3/Activities/Login.java
+++ app/src/main/java/com/example/mktabty_attempt3/Activities/Login.java
@@ -1,20 +1,30 @@
 package com.example.mktabty_attempt3.Activities;
 
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.mktabty_attempt3.R;
 
 public class Login extends AppCompatActivity {
-
+    EditText editText1,editText2;
+    Button b1,b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Button b = findViewById(R.id.button3);
+        TextView t = findViewById(R.id.textView);
+//        editText1 =findViewById(R.id.editText);
+        editText2 = findViewById(R.id.pass_reg_et);
+        b1=findViewById(R.id.button3);
+        b2=findViewById(R.id.button4);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,5 +32,12 @@ public class Login extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        Typeface face = Typeface.createFromAsset(getAssets(),"font1.otf");
+        Typeface face1 = Typeface.createFromAsset(getAssets(),"font2.otf");
+
+        t.setTypeface(face);
+        b1.setTypeface(face);
+        b2.setTypeface(face1);
+//        editText2.setTypeface(face);
     }
 }
