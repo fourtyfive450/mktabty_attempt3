2db0c060828b5941184237ecf1f675925b9c68b9
Four <fourtyfive450@gmail.com>
2019-04-04 17:41:38 +0200

    Update FragmentProfile.java

--- app/src/main/java/com/example/mktabty_attempt3/Fragments/FragmentProfile.java
+++ app/src/main/java/com/example/mktabty_attempt3/Fragments/FragmentProfile.java
@@ -1,6 +1,7 @@
 package com.example.mktabty_attempt3.Fragments;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.mktabty_attempt3.R;
 
@@ -45,11 +47,32 @@ public class FragmentProfile extends Fragment {
 
     RecyclerView recyclerView;
     DrawerLayout mDrawerLayout;
-
+    TextView label,t,t1,t2,t3,t4,t5,t6,t8,t9,t10;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_fragment_profile, container, false);
+        Typeface face = Typeface.createFromAsset(getActivity().getAssets(),"font1.otf");
+        Typeface face1 = Typeface.createFromAsset(getActivity().getAssets(),"font2.otf");
+        label=view.findViewById(R.id.textView3);
+        label.setTypeface(face1);
+        t1=view.findViewById(R.id.t1);
+        t2=view.findViewById(R.id.t2);
+        t3=view.findViewById(R.id.t3);
+        t4=view.findViewById(R.id.t1);
+        t5=view.findViewById(R.id.t1);
+        t6=view.findViewById(R.id.t1);
+        t8=view.findViewById(R.id.t1);
+        t9=view.findViewById(R.id.t1);
+        t10=view.findViewById(R.id.t10);
+        t2.setTypeface(face);
+        t3.setTypeface(face);
+        t4.setTypeface(face);
+        t5.setTypeface(face);
+        t6.setTypeface(face);
+        t8.setTypeface(face);
+        t9.setTypeface(face);
+        t10.setTypeface(face);
 
         mDrawerLayout = (DrawerLayout)getActivity().findViewById(R.id.drawer_layout);
         Button button = view.findViewById(R.id.b4);
